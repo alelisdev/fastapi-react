ef5ad569dfa733c4184cba7cde477f7e4a892726
WEB/BLOCKCHAIN WORLD <92005974+topcoder728@users.noreply.github.com>
2022-07-12 03:46:02 +0900

    fix: backend

--- backend/app/jwttoken.py
+++ backend/app/jwttoken.py
@@ -12,13 +12,3 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
-
-# def verify_token(token:str,credentials_exception):
-# 	try:
-# 		payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-# 		username: str = payload.get("sub")
-# 		if username is None:
-# 			raise credentials_exception
-# 		token_data = TokenData(username=username)
-# 	except JWTError:
-# 	    raise credentials_exception
